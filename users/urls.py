2456702ea0a10a505a5487bfad54571be89a7cb8
rashed <x.suwaidi@gmail.com>
2020-11-21 20:23:48 +0400

    added custom account creation

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.conf.urls import include, url
 
 from . import views
 
 app_name = 'users'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('', include("django.contrib.auth.urls")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,26 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect,reverse
+from users.forms import CustomUserCreationForm
+from django.contrib.auth import login
+
 
 def index(request):
     return HttpResponse("You're looking at the index page")
+
+
+def profile(request):
+    return render(request, "users/profile.html")
+
+
+def register(request):
+    if request.method == "GET":
+        return render(
+            request, "users/register.html",
+            {"form": CustomUserCreationForm}
+        )
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("users:profile"))
