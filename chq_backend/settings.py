73a407a91da50b654b6d1fce85adaf168fee869d
rashed <x.suwaidi@gmail.com>
2020-11-21 20:22:43 +0400

    login and logout redirect to custom page

--- chq_backend/settings.py
+++ chq_backend/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Redirect after login
+LOGIN_REDIRECT_URL = "users:profile"
+
+# Redirect after logout
+LOGOUT_REDIRECT_URL = "users:profile"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
