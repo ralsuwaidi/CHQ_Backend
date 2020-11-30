dbcac22302330104b5107ec0e828010b62750d34
rashed <x.suwaidi@gmail.com>
2020-11-30 17:09:26 +0400

    throttle added

--- chq_backend/settings.py
+++ chq_backend/settings.py
@@ -61,7 +61,15 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
          'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/minute',
+        'user': '20/minute'
+    }
 }
 
 ROOT_URLCONF = 'chq_backend.urls'
