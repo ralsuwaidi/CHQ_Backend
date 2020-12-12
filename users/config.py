88665ba166b50d018d20e9eefa5244f4d0a1f205
rashed <x.suwaidi@gmail.com>
2020-12-12 15:52:42 +0400

    added config location

---
+++ users/config.py
@@ -0,0 +1,14 @@
+
+
+NEWS_SITES = {
+    "codinghorror": "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fblog.codinghorror.com%2Frss%2F",
+    "lambda": "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Flambda-the-ultimate.org%2Frss.xml",
+    "bliki": "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fmartinfowler.com%2Fbliki%2Fbliki.atom",
+    "joe": "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.joelonsoftware.com%2Frss.xml",
+    "feed": "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.feedburner.com%2Ffreetechbooks",
+    "orilly": "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fradar.oreilly.com%2Findex.rdf",
+    "paul": "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.feedburner.com%2FPaulGrahamUnofficialRssFeed",
+    "reddit_programming": "https://www.reddit.com/r/programming/.json"
+}
+
+DEFAULT_NEWS = "lambda"
